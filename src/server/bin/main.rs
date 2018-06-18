780a40d31735b33cd303eb8acaec127649cb2412
James Haver <mchaver@gmail.com>
2018-06-18 23:10:12 +0800

    Clean up

--- src/server/bin/main.rs
+++ src/server/bin/main.rs
@@ -19,13 +19,30 @@ extern crate base64;
 use std::u8;
 use self::base64::{decode};
 
-
+use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 
+use std::path::{Path, PathBuf};
+use rocket::response::NamedFile;
+
+
+extern crate image;
+use image::GenericImage;
+use image::FilterType;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DigitImage {
     image_base64: String,
+}
+
+#[get("/")]
+fn index() -> io::Result<NamedFile> {
+    NamedFile::open("static/index.html")
+}
+
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
 /*
@@ -37,44 +54,46 @@ prediction = model.predict(image)
 
 #[post("/image", format = "application/json", data = "<image>")]
 fn image(image: Json<DigitImage>) -> &'static str {
+    println!("image called");
     let split = image.image_base64.split(",");
     let vec = split.collect::<Vec<&str>>();
     if vec.len() > 0 {
         match decode(vec[1]) {
             Ok(img_data) => {
-                match File::create("test.jpg") {
-                    Ok(mut buffer) => { buffer.write(&img_data); () },
-                    Err(err) => (),
+
+
+                //let buffer: &[u8] = ...; // Generate the image data
+
+                // Save the buffer as "image.png"
+                // image::save_buffer(&Path::new("image.png"), buffer, 800, 600, image::RGBA(8))
+                // image::save_buffer(&Path::new("test2.jpg"), &img_data, 200, 200, image::RGB(8));
+                
+                match File::create("test1.png") {
+                    Ok(mut buffer) => {
+                        buffer.write(&img_data);
+                        println!("open test1.png");
+                        match image::open("test1.png") {
+                            Ok(imgg) => {
+                                println!("opened");
+                                let ref mut ab = imgg.resize_exact(28,28, FilterType::Nearest);
+                                // let ref mut fout = File::create("test2.png").unwrap();
+                                // Write the contents of this image to the Writer in PNG format.
+                                ab.save("test2.png").unwrap();
+                                ab.save("test2.jpg").unwrap();
+                                ()
+                            },
+                            Err(err) => println!("{}", err),
+                        }
+                    },
+                    Err(err) => println!("{}", err),
                 }},
-            Err(err) => (),
+            Err(err) => println!("{}", err),
         }
         // Writes some prefix of the byte string, not necessarily all of it.
     }
     "hello"
 }
 
-// extern crate models;
-// use models::{DigitImage};
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
-#[get("/hello/<name>")]
-fn hello(name: &RawStr) -> String {
-    format!("Hello, {}!", name.as_str())
-}
-
-#[get("/hyo/<name>/<age>/<cool>")]
-fn halo(name: String, age: u8, cool: bool) -> String {
-    if cool {
-        format!("You're a cool {} year old, {}!", age, name)
-    } else {
-        format!("{}, we need to talk about your coolness.", name)
-    }
-}
-
 fn main() {
-    rocket::ignite().mount("/", routes![index, hello, halo, image]).launch();
+    rocket::ignite().mount("/", routes![index, image, files]).launch();
 }
